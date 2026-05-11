fa3609dfb584d217ed1051f99bbe9a34a7d41a52
Jawairia <Jawairiamazhar21@gmail.com>
2026-05-11 23:47:26 +0500

    feat: enhance home page layout with horizontally scrollable movie cards and updated styling

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,26 +1,28 @@
 import React from 'react'
 import background from '../assets/hero-bg.png'
 import '../App.css'
-import.meta.env.VITE_TMDB_API_KEY
+import.meta.env.VITE_TMDB_API_KEY 
 
 const home = () => {
   const [movies, setMovies] = React.useState([])
 
-  React.useState(() => {
-    fetch(`https://api.themoviedb.org/3/trending/all/day?api_key=${import.meta.env.VITE_TMDB_API_KEY}`)
+  React.useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/trending/movie/week?qty=5&api_key=${import.meta.env.VITE_TMDB_API_KEY}`)
     .then(res => res.json())
     .then(data => {
       console.log(data.results)
       setMovies(data.results)
     })
-  })
+  }, [])
 
   return (
     <>
-      <main>
-        <img src={background} alt="" className='tint w-full h-full object-cover'/>
+      <main className='relative h-screen'>
+        <img src={background} alt="" className='w-full h-full object-cover absolute inset-0 bg-black/60'/>
+        <div className='absolute inset-0 bg-black/60'></div>
+
         <div className='flex flex-col items-center justify-center text-center gap-4 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white'>
-          <h1 className='text-5xl bold relative'>Unlimited Movies, TV Shows and More</h1>
+          <h1 className='text-6xl bold relative font-[900]'>Unlimited Movies, TV Shows and More</h1>
           <p className='relative'>Starts at 250. Cancel anytime.</p>
           <span>Ready to watch? Enter your email to create or restart your membership.</span>
           <div className='flex flex-row'>
@@ -30,18 +32,19 @@ const home = () => {
         </div>
       </main>
 
-{/* Trending       */}
-      <div className='flex w-full px-30 justify-center items-center flex-wrap gap-4'>
+{/* Trending*/}
+      <section className='px-34 py-4'>
         <span className='w-full text-xl font-bold'>Trending Now</span>
-        {movies.map(movie => (
-          <div key={movie.id} className='flex flex-col items-center justify-center text-center gap-2'>
-            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title || movie.name} className='w-42 h-42'/>
-            <h2>{movie.title || movie.name}</h2>
-          </div>
-        ))}
-      </div>
+        <div className='scroll-menu '>
+          {movies.map(movie => (
+            <div key={movie.id} className='movie-card w-44 h-full flex flex-col items-center justify-center gap-2 rounded-md'>
+              <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title || movie.name} className='w-full h-full object-cover rounded'/>
+            </div>
+          ))}
+        </div>
+      </section>
 
-{/* More Reasons to join       */}
+{/* More Reasons to join*/}
       <div>
 
       </div>
