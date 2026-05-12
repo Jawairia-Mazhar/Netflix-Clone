c9a183f2159c5c56c08a587ad08515c231ab5d4b
Jawairia <Jawairiamazhar21@gmail.com>
2026-05-12 17:30:48 +0500

    feat: add .env file for API key and update navbar styling and layout

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,9 +3,9 @@ import logo from '../assets/netflix.png'
 
 const navbar = () => {
   return (
-    <nav className='flex items-center justify-items-center justify-between bg-transparent absolute w-full px-30 z-10'>
-        <img src={logo} alt="Netflix" className='w-24 h-24' />
-        <button className='bg-red-500 text-white px-4 py-2 rounded'>Sign In</button>
+    <nav className='flex items-center justify-items-center justify-between bg-transparent absolute w-full px-34 py-4 z-10'>
+        <img src={logo} alt="Netflix" className='w-36' />
+        <button className='bg-red-500 text-white px-4 py-1 rounded text-sm text-center font-semibold'>Sign In</button>
     </nav>
   )
 }
