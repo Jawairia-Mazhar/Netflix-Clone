deac4f458623f1fbb9c69324daecb2e1f58693ba
Jawairia <Jawairiamazhar21@gmail.com>
2026-05-16 23:36:00 +0500

    style: enhance styling for ReasonsToJoin component with gradient background and improved text formatting

--- src/components/ReasonsToJoin.jsx
+++ src/components/ReasonsToJoin.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const ReasonsToJoin = ({ title, description, icon }) => {
   return (
-    <div className='bg-gray-400 rounded-md p-2 flex flex-col text-left gap-2 items-start'>
-        <h3>{title}</h3>
-        <p>{description}</p>
-        <span className=''>{icon}</span>
+    <div className='relative bg-linear-to-br from-blue-950 to-rose-950 rounded-md p-4 pb-12 text-left gap-2 w-full max-w-sm h-68'>
+        <h3 className = "text-2xl text-white font-bold">{title}</h3>
+        <p className='text-gray-200 py-2'>{description}</p>
+        <span className='absolute right-4 bottom-4 text-2xl'>{icon}</span>
     </div>
   )
 }
