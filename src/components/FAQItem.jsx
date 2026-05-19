fa58df01a99f1cbeaa313ed3e226a6a31fb12180
Jawairia <Jawairiamazhar21@gmail.com>
2026-05-19 19:02:13 +0500

    style: enhance FAQItem and ReasonsToJoin components with improved hover effects and responsive design

--- src/components/FAQItem.jsx
+++ src/components/FAQItem.jsx
@@ -4,10 +4,10 @@ export const FAQItem = ( { question, answer, isOpen } ) => {
   const [open, setOpen] = React.useState(isOpen) // Local state to track if this FAQ item is open or closed
   return (
     <div className='text-white'>  
-      <div className='flex justify-between items-center bg-zinc-600 p-4 cursor-pointer'
+      <div className='flex justify-between items-center bg-zinc-600 p-4 cursor-pointer hover:bg-zinc-500'
        onClick={() => setOpen(!open)} > {/* Toggle the open state when the FAQ item is clicked */}
         <h3 className="text-2xl">{question}</h3>
-        <button className='w-8 h-8 text-3xl items-center smooth-transition'>{open ? "×" : "+"}</button>
+        <button className='flex w-8 h-8 items-center justify-center text-3xl transition-all duration-200'>{open ? "×" : "+"}</button>
       </div>
       
       {open && 
--- src/components/ReasonsToJoin.jsx
+++ src/components/ReasonsToJoin.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ReasonsToJoin = ({ title, description, icon }) => {
   return (
-    <div className='relative bg-linear-to-br from-blue-950 to-rose-950 rounded-md p-4 pb-12 text-left gap-2 w-full max-w-sm h-68'>
+    <div className='relative bg-linear-to-br from-blue-950 to-rose-950 rounded-md p-4 md:pb-12 text-left gap-2 w-full md:max-w-sm max-w-lg h-68'>
         <h3 className = "text-2xl text-white font-bold">{title}</h3>
         <p className='text-gray-200 py-2'>{description}</p>
         <span className='absolute right-4 bottom-4 text-2xl'>{icon}</span>
